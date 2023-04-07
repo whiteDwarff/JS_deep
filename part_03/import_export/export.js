a361207f854cc4eff6596b9e879e2744c608cd0f
whiteDwraf <munstarrrrr@gmail.com>
2023-04-07 20:53:18 +0900

    0407 commit

--- part_03/import_export/export.js
+++ part_03/import_export/export.js
@@ -3,10 +3,21 @@ const [b,c,d,e] = [10,20,30,40];
 
 const plus = (a,b) => a+b;
 
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    set setAge(age) {
+        this.age += age;
+    }
+    get getAge() { return this.age; }
+}
+
 
 // default는 1회만, 하나의 변수만 가능
 export default a;
 
 // 여러개를 export
 export {b,c,d,e};
-export {plus}
+export {plus, Human}
